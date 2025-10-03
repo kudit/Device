0bbb302a669e8e1bf4b6d3029cab5ed66573a69a
kudit <support@kudit.com>
2025-10-03 00:06:44 -0400

    v2.10.5
    Updated Compatibility & Color to improve WASM compatiblity.

--- Sources/Device.swift
+++ Sources/Device.swift
@@ -19,7 +19,7 @@
 
 public extension Device {
     /// The version of the Device Library since cannot get directly from Package.
-    static let version: Version = "2.10.4"
+    static let version: Version = "2.10.5"
 }
 import Compatibility
 
